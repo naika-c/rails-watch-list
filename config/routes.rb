824e525543c3cab3061c2b2bd6993611bf39acf3
Naika Estriplet <naika.c.3113@gmail.com>
2025-05-23 14:58:39 -0400

    added detroy route lists

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   root "lists#index"
-  resources :lists, only: [:show, :create, :new, :delete] do
-    resources :bookmarks, only: [:create, :new, :delete]
+  resources :lists, only: [:show, :create, :new, :destroy] do
+    resources :bookmarks, only: [:create, :new]
   end
   resources :bookmarks, only: [:destroy]
   resources :movies, only: [:index]
