397aaf3913977c4a98059b3303d584d833b9f680
Naika Estriplet <naika.c.3113@gmail.com>
2025-05-23 11:32:02 -0400

    added a container to the body of the main application hltml file

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
-  # root "posts#index"
-  resources :lists, only: [:index, :show, :create, :new, :destroy] do
+  root "lists#index"
+  resources :lists, only: [:show, :create, :new, :delete] do
     resources :bookmarks, only: [:create, :new, :delete]
   end
   resources :bookmarks, only: [:destroy]
